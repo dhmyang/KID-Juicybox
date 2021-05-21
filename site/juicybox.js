0af2a29053f58486826225a32f6f8a9f99dd70a8
dhmyang <55231982+dhmyang@users.noreply.github.com>
2021-05-21 04:01:30 +0200

    site

---
+++ site/juicybox.js
@@ -0,0 +1,19 @@
+// ---------------\\
+//    Swiper
+// ---------------//
+var swiper = new Swiper(".swiper-container", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+// ---------------\\
+//    Navbar
+// ---------------//
